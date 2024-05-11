d17d46e891a65b2bd289148c0ca903726a4a4cfe
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-05-11 06:11:45 -0500

    show correct html based on url

---
+++ index.js
@@ -0,0 +1,36 @@
+var http = require('http');
+const fs = require('fs');
+
+
+
+http.createServer(function (req, res) {
+  const url = req.url;
+
+  if (url === '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  } else if (url === '/about') {
+    fs.readFile('about.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  } else if (url === '/contact-me') {
+    fs.readFile('contact-me.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  } else {
+    fs.readFile('404.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    });
+  }
+
+
+}).listen(8080);
